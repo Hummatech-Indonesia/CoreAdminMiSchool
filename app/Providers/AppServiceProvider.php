c360552394d2ac9d39b71b500b1899a90f7c0ae6
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-07-19 09:10:33 +0700

    city backend

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,23 @@
 
 namespace App\Providers;
 
+use App\Contracts\Interfaces\CityInterface;
+use App\Contracts\Repositories\CityRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    private array $register = [
+        CityInterface::class => CityRepository::class,
+    ];
     /**
      * Register any application services.
      */
     public function register(): void
     {
-        //
+        foreach ($this->register as $index => $value) {
+            $this->app->bind($index, $value);
+        }
     }
 
     /**
@@ -19,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        config(['app.locale' => 'id']);
+        \Carbon\Carbon::setLocale('id');
     }
 }
