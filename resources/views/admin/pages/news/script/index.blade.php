19e471c43e0aab611a645a0c4ad1fd14db6582df
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-08-06 13:33:22 +0700

    preview image create berita

--- resources/views/admin/pages/news/script/index.blade.php
+++ resources/views/admin/pages/news/script/index.blade.php
@@ -66,3 +66,16 @@
       ]
     });
 </script>
+
+<script>
+  document.getElementById('formFile').addEventListener('change', function(event) {
+      const file = event.target.files[0];
+      if (file) {
+          const reader = new FileReader();
+          reader.onload = function(e) {
+              document.getElementById('previewImage').src = e.target.result;
+          };
+          reader.readAsDataURL(file);
+      }
+  });
+</script>
