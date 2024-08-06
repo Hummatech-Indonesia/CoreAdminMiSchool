75e2c1a0a09115354bf9af4ef94a8ec8ce79590e
nesaathussholeha <nesaathussholeha@gmail.com>
2024-08-06 15:20:35 +0700

    no data kategori berita

--- resources/views/admin/pages/news/category/index.blade.php
+++ resources/views/admin/pages/news/category/index.blade.php
@@ -100,7 +100,16 @@
                     </tr>
                 @empty
                     <tr>
-                        <td colspan="4">Tidak ada data</td>
+                        <td colspan="4">
+                            <div class="d-flex flex-column justify-content-center align-items-center">
+                                <img src="{{ asset('admin_assets/dist/images/empty/no-data.png') }}" alt=""
+                                    width="300px">
+                                <p class="fs-5 text-dark text-center mt-2">
+                                    Kategori belum ditambahkan
+                                </p>
+                            </div>
+                        </td>
+
                     </tr>
                 @endforelse
             </tbody>
