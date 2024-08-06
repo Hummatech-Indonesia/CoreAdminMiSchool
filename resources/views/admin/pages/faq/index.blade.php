11ecdc774296eb57db1c41c5e09fed69260a2e16
nesaathussholeha <nesaathussholeha@gmail.com>
2024-08-06 15:32:51 +0700

    update tampilan faq

--- resources/views/admin/pages/faq/index.blade.php
+++ resources/views/admin/pages/faq/index.blade.php
@@ -1,19 +1,27 @@
 @extends('admin.layouts.app')
-
+@section('style')
+    <style>
+        .breadcrumb-item+.breadcrumb-item::before{
+            color: white;
+        }
+    </style>
+@endsection
 @section('content')
-    <div class="card bg-light-info shadow-none position-relative overflow-hidden">
+    <div class="card bg-primary shadow-none position-relative overflow-hidden">
         <div class="card-body px-4 py-3">
             <div class="row align-items-center">
                 <div class="col-9">
-                    <h4 class="fw-semibold mb-8">FAQ</h4>
+                    <h4 class="fw-semibold mb-8 text-light" style="color: white;">FAQ</h4>
                     <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item"><a class="text-muted text-decoration-none" href="index-2.html">Home</a>
+                        <ol class="breadcrumb" style="color: white;">
+                            <li class="breadcrumb-item" style="color: white;">
+                                <a class="text-decoration-none" style="color: white;" href="index-2.html">Home</a>
                             </li>
-                            <li class="breadcrumb-item" aria-current="page">FAQ</li>
+                            <li class="breadcrumb-item" aria-current="page" style="color: white;">FAQ</li>
                         </ol>
                     </nav>
                 </div>
+
                 <div class="col-3">
                     <div class="text-center mb-n5">
                         <img src="{{ asset('admin_assets/dist/images/breadcrumb/ChatBc.png') }}" alt=""
